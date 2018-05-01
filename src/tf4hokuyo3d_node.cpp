a456693a047699daf240031824bc4ea8e9e66cf1
MasaakiWatanabe <masaakiwtnb@gmail.com>
2018-05-01 13:35:47 +0900

    update

--- src/tf4hokuyo3d_node.cpp
+++ src/tf4hokuyo3d_node.cpp
@@ -1,95 +1,69 @@
 #include "ros/ros.h"
-#include <sensor_msgs/PointCloud.h>
-#include <sensor_msgs/PointCloud2.h>
+#include "tf/transform_listener.h"
+#include "sensor_msgs/PointCloud.h"
+#include "sensor_msgs/PointCloud2.h"
 #include <sensor_msgs/point_cloud_conversion.h>
-#include <tf/transform_listener.h>
+#include "tf/message_filter.h"
+#include "message_filters/subscriber.h"
+#include "laser_geometry/laser_geometry.h"
 
-sensor_msgs::PointCloud2 msg;
-sensor_msgs::PointCloud msg1;
-tf::TransformListener listener;
+class TransformHokuyo3dPointCloud{
 
-class tflistener{
-	private:
-		tf::TransformListener listener_;
+public:
 
-	public:
-		ros::Subscriber sub;
-		ros::NodeHandle n;
+  ros::NodeHandle n_;
+  tf::TransformListener listener_;
+  ros::Publisher scan_pub_;
+  ros::Subscriber scan_sub_;
 
-		// Subscribeする対象のトピックが更新されたら呼び出されるコールバック関数
-		// 引数にはトピックにPublishされるメッセージの型と同じ型を定義する
-		void tfCallback(const sensor_msgs::PointCloud a_msg)
-		{
-		    //tf::TransformListener listener;
-		    tf::StampedTransform transform;
-		    ROS_INFO("Callback");
-		    try{
-		      ROS_INFO("Receive time=%u.%u, width=%d", a_msg.header.stamp.sec, a_msg.header.stamp.nsec, msg.width);
+  TransformHokuyo3dPointCloud(ros::NodeHandle n):
+    n_(n)
+  {
+    scan_pub_ = n_.advertise<sensor_msgs::PointCloud2>("/points_raw",1);
+    scan_sub_ = n_.subscribe("/hokuyo3d/hokuyo_cloud", 10, &TransformHokuyo3dPointCloud::scanCallback, this);
+  }
 
-		      //listener.lookupTransform("/hokuyo3d", "/robot_pose", a_msg.header.stamp, transform);
-		      listener_.waitForTransform("/hokuyo3d", "/robot_pose", a_msg.header.stamp, ros::Duration(0.05));
-		      ROS_INFO("Callback123");
-		      listener_.transformPointCloud("hokuyo3d", a_msg.header.stamp, a_msg, "robot_pose", msg1);
-		      ROS_INFO("Callback4456");
-		      sensor_msgs::convertPointCloudToPointCloud2(msg1, msg);
-		      //msg = msg1;
-		      ROS_INFO("Callback789");
+  void scanCallback (const sensor_msgs::PointCloud scan_in)
+  {
+    sensor_msgs::PointCloud msg1;
+    sensor_msgs::PointCloud2 cloud;
+    try
+    {
+      ROS_INFO("Receive time=%u.%u", scan_in.header.stamp.sec, scan_in.header.stamp.nsec);
 
-		    }
-		    catch (tf::TransformException &ex) {
-		      ROS_ERROR("%s",ex.what());
-		      ros::Duration(0.1).sleep();
-		    }
-		      ROS_INFO("Callback end");
-		}
+      listener_.waitForTransform("/hokuyo3d", "/robot_pose", scan_in.header.stamp, ros::Duration(0.05));
+      ROS_INFO("Callback123");
+      listener_.transformPointCloud("hokuyo3d", scan_in.header.stamp, scan_in, "robot_pose", msg1);
+      ROS_INFO("Callback4456");
+      sensor_msgs::convertPointCloudToPointCloud2(msg1, cloud);
+      ROS_INFO("Callback789");
 
+    }
+    catch (tf::TransformException& e)
+    {
+        std::cout << e.what();
+        return;
+    }
 
-		tflistener(){
-			  sub = n.subscribe("/hokuyo3d/hokuyo_cloud", 10, tfCallback);
-		};
-
-		~tflistener();
-
+    scan_pub_.publish(cloud);
+  }
 };
 
-
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
+
   // 初期化のためのAPI
   ros::init(argc, argv, "tf4hokuyo3d");
   // ノードハンドラの宣言
-  //ros::NodeHandle n;
-  //クラス生成
-  tflistener tflistener_c;
+  ros::NodeHandle n;
 
-  // Subscriberとしてpara_inputというトピックがSubscribeし、トピックが更新されたときは
-  // chatterCallbackという名前のコールバック関数を実行する
-  //ros::Subscriber sub = n.subscribe("/hokuyo3d/hokuyo_cloud", 10, tflistener_c.tfCallback);
+  TransformHokuyo3dPointCloud tf4hokuyo3d_node(n);
 
-  ros::Publisher pub = tflistener_c.n.advertise<sensor_msgs::PointCloud2>("point_raw", 10);
+  ROS_INFO("Loop Start.");
 
-  ros::Rate loop_rate(200);
+  ros::spin();
 
-
-  ROS_INFO("MAIN LOOP");
-  int count = 0;
-  while (ros::ok())//ノードが実行中は基本的にros::ok()=1
-  {
-    ros::spinOnce();
-
-    //printf("LOOP=%d\n", count);
-	    ROS_INFO("LOOP");
-
-    if(tflistener_c.sub){
-	    ROS_INFO("pub");
-      pub.publish(msg);//PublishのAPI
-    }
-
-    loop_rate.sleep();
-
-    count++;
-  }
-
+  ROS_INFO("Loop End.");
 
   return 0;
 }
